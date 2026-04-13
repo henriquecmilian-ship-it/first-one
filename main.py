ebd770cee2feb1c1622b8a1e1e57f514e4cca2f2
henriquecmilian-ship-it <henriquecmilian@gmail.com>
2026-04-13 19:18:49 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+
+fatos=['>>> Gosto muito de jogar video game <<<',
+       '>>> Gosto muito de ver comedia <<<',
+       '>>> Meu jogo preferido é God Of War III <<<',
+       '>>> Meu filme favorito é Corra que a policia vem ai 2 e meio <<<']
+ 
+print(random.choice(fatos))
