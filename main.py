390ede613a938e9679de24712f1dd311c971505f
henriquecmilian-ship-it <henriquecmilian@gmail.com>
2026-04-27 15:48:09 -0300

    Um Pouco Sobre Mim
    neste codigo tem poucas informações mas fala ligeramente sobre e tem alguns fatos sobre mim

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 import random
 
-caracteres="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?/~`"
-
-quantidade=input("Digite a quantidade de caracteres para a senha: ")
-
-senha=""
-
-for quantidade in range(int(quantidade)):
-    senha+=random.choice(caracteres)
-print("Senha gerada:",'{}'.format(senha))
-
-gotou=input=('está satisfeito com a senha gerada? (s/n)')
-
-
-if gotou=='s':
-    print("perfeito")
-elif gotou=='n':
-    novamente=input=("quer gerar uma nova senha?")
-    
-    if novamente=='s':
-        senha=""
-        for quantidade in range(int(quantidade)):
-            senha+=random.choice(caracteres)
-        print("Nova senha gerada:",'{}'.format(senha))
-    elif novamente=='n':
-        print("Adeus!")
+fatos=['>>> Gosto muito de jogar video game <<<',
+       '>>> Gosto muito de ver comedia <<<',
+       '>>> Meu jogo preferido é God Of War III <<<',
+       '>>> Meu filme favorito é Corra que a policia vem ai 2 e meio <<<']
+ 
+print(random.choice(fatos))
